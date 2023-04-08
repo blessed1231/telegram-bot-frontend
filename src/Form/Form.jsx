4f8ab67ce962cc56c01b60a25bde445af50428f6
blessed1231 <ivankraten@gmail.com>
2023-04-08 13:37:04 +0200

    #

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -17,9 +17,9 @@ const Form = () => {
     })
 
     useEffect(() => {
-        tg.WebApp.onEvent('mainButtonClicked', onSendData)
+        tg.onEvent('mainButtonClicked', onSendData)
         return () => {
-            tg.WebApp.offEvent('mainButtonClicked', onSendData)
+            tg.offEvent('mainButtonClicked', onSendData)
         }
     } ,[])
 
